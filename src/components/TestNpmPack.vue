a0e3b563f40a611b9a988eaf6377e13b563b9510
xw <xiaowenb@zbj.com>
2018-05-22 20:16:16 +0800

    测试打包发布

---
+++ src/components/TestNpmPack.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="test-npm-pack">
+        <div>测试打包</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TestNpmPack',
+    data () {
+      return {
+        msg: ''
+      }
+    },
+    mounted(){
+    },
+    methods:{
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
+   
